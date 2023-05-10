c55bca793c9c717061893e4b5800ac26e684fa09
css22-076 <css22200076@gmail.com>
2023-05-10 22:30:16 +0900

    Implement saveUser in user.c

--- User/user.c
+++ User/user.c
@@ -1,2 +1,15 @@
 #include "user.h"
 #include "stdio.h"
+#define max_size = 20;
+
+void saveUser(User *u[], int count){
+    FILE *data;
+    data = fopen("user.txt", "w");
+    for(int i=0; i<count; i++){
+        if(u[i]->id!=0){
+            fprintf(data,"%s %s %s %s\n", u[i]->id, u[i]->password, u[i]->phoneNumber, u[i]->userName);
+        }
+    }
+    printf("=> 저장되었습니다!\n");
+    fclose(data);
+}
