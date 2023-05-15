f43df41baa4464d587ba3033e58f950a384ee8a4
csee22-076 <csee22200076@gmail.com>
2023-05-15 12:38:02 +0900

    Implement signUP in user.c

--- User/user.c
+++ User/user.c
@@ -43,3 +43,65 @@ void withdrawal(User *u[], int count){
     }
     printf("=>찾으시는 아이디가 없습니다.\n");
 }
+
+void signUp(User *u[]){
+    char check_blank = 0;
+    char id[10];
+    char password[12];
+    char phoneNumber[11];
+    char userName[30]; 
+
+    while(1){
+        printf("아이디(10글자 이내, 띄어쓰기 미포함): ");
+        scanf("%[^\n]s", id);
+        if(strlen(id)>10){
+            printf("=>10글자 초과입니다. 다시 시도해주세요.\n");
+            continue;
+        }
+        for(int i=0; i<10; i++){
+            if(strcmp(id[i], " ")==0){
+                printf("=>띄어쓰기가 되어 있습니다. 다시 시도해주세요.\n");
+                check_blank = 1;
+                break;
+            }
+        }
+        if(check_blank==1) continue;
+        else break;
+    }
+    check_blank = 0;
+
+    while(1){
+        printf("비밀번호(12글자 이내, 띄어쓰기 미포함): ");
+        scanf("%[^\n]s", password);
+        if(strlen(password)>!2){
+            printf("=>12글자 초과입니다. 다시 시도해주세요.\n");
+            continue;
+        }
+        for(int i=0; i<12; i++){
+            if(strcmp(password[i], " "==0)){
+                printf("=>띄어쓰기가 되어 있습니다. 다시 시도해주세요.\n");
+                check_blank = 1;
+                break;
+            }
+        }
+        if(check_blank==1) continue;
+        else break;
+    }
+
+    while(1){
+        printf("전화번호(-없이 숫자만, 띄어쓰기 미포함): ");
+        scanf("%[^\n]s", phoneNumber);
+        if(strlen(password)>12){
+            printf("=>11글자 초과 혹은 '-'이나 띄어쓰기가 포함되어 있습니다. 다시 시도해주세요.\n");
+            continue;
+        }
+        else break;
+    }
+
+    printf("이름: ");
+    scanf("%[^\n]s", userName);
+
+    printf("=>회원가입이 완료되었습니다.\n");
+}
+
+void signIn(User *u[]);
